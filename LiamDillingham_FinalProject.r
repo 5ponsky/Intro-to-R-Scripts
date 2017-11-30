d4861ab9a8a7bd321edf416c73a356ebb8fc50ee
Liam Dillingham <mailgd9@gmail.com>
2017-11-30 14:48:54 -0600

    asdf

--- LiamDillingham_FinalProject.r
+++ LiamDillingham_FinalProject.r
@@ -18,57 +18,27 @@ boxplot(admission[,3], col="blue", main="GPA")
 shapiro.test(admission$GRE)
 shapiro.test(admission$GPA)
 
+# QQ normality
 qqnorm(admission$GRE, ylab="GRE Scores",main="QQ GRE")
 qqnorm(admission$GPA, ylab="GPA", main="QQ GPA")
 
-
-#logistic model
-ind=sample(2,nrow(admission),replace=TRUE,prob=c(0.8,0.2))
-tdata=admission[ind==1,]
-vdata=admission[ind==2,]
-
-model=glm(admit~GRE+GPA,data=tdata,family=binomial)
+# model fitting
+model=glm(admit~GRE+GPA,data=admission,family="binomial")
 summary(model)
+
+# 
+# for each predictor in the model, the coefficient is the ratio
+# between the response level, and the reference event, assuming the other 
+# predictors remain constant
 
 # predict
 pred=predict(model,vdata,type="response")
 
-newdata=data.frame(GPA=3.4,GRE=600)
+newdata=data.frame(GPA=3.4,GRE=650)
 newpred=predict(model,newdata,type="response")
+newpred
 
-# misclassification error
-model_pre_admit=rep("0",75)
-model_pred_admit[pred>0.5]="1"
-tab=table(model_pred_admit, vdata$admit)
-print(tab)
-1-sum(diag(tab))/sum(tab)
+# A new applicant has a 34% chance of being accepted according to the model
 
 
 
-
-
-
-
-
-# Create training data
-admitted=admission[which(admission$admit == 1),]
-notadmit=admission[which(admission$admit == 0),]
-set.seed(100) #for repeatability of samples
-
-admitted_training_rows=sample(1:nrow(admitted), 0.7*nrow(admitted))
-notadmit_training_rows=sample(1:nrow(notadmit), 0.7*nrow(notadmit))
-
-training_admit=admitted[admitted_training_rows,]
-training_notadmit=notadmit[notadmit_training_rows,]
-trainingdata=rbind(training_admit,training_notadmit)
-
-#Create test data
-test_admit=admitted
-
-model=glm(admit~GPA+GRE,data=admission,family=binomial)
-summary(model)
-
-prediction=predict(model,type="response")
-prediction
-
-table(admission$admit, predict>0.5)
